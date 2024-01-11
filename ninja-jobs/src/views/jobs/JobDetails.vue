52937797c0c862f91726b3b2af097b82aabad82e
Chan Kit Yuen <chankityuen98@gmail.com>
2024-01-11 22:43:47 +0800

    Ninja jobs

--- ninja-jobs/src/views/jobs/JobDetails.vue
+++ ninja-jobs/src/views/jobs/JobDetails.vue
@@ -1,6 +1,7 @@
 <template>
-  <h1>Job Details Page</h1>
+  <h1>{{ job.title }}</h1>
   <p>Job ID: {{ id }}</p>
+  <p>{{ job.details }}</p>
   <!-- <p>Job Title: {{ title }}</p>
   <p>Job Details: {{ details }}</p> -->
 </template>
@@ -8,6 +9,17 @@
 <script>
 export default {
   props: ["id"],
+  data() {
+    return {
+      job: null,
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs/" + this.id)
+      .then((res) => res.json())
+      .then((data) => (this.job = data))
+      .catch((err) => console.log(err));
+  },
   //   data() {
   //     return {
   //       id: this.$route.params.id,
